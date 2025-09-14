baac698f17fca414806dfaecc33d41a46c676f66
Navid <nh222yr@student.lnu.se>
2025-09-14 16:06:16 +0200

    add main RandomGenerator class

---
+++ src/RandomGenerator.js
@@ -0,0 +1,9 @@
+class randomGenerator {
+  constructor () {
+    this.passwordGenerator = new passwordGenerator ()
+
+  }
+  generatePassword (length, options) {
+    return this.passwordGenerator.generatorSecure(length, options)
+  }
+}
