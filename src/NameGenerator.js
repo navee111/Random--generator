f28bcf5c28669c9089a88655e24426308b3e61e8
Navid <nh222yr@student.lnu.se>
2025-09-16 19:28:30 +0200

    created NameGenerator class

---
+++ src/NameGenerator.js
@@ -0,0 +1,35 @@
+import {
+  FIRST_NAMES,
+  LAST_NAMES,
+  USERNAMES_PREFIXES,
+  USERNAMES_SUFFIXES,
+  BUSINESS_WORDS,
+  BUSINESS_SUFFIXES
+} from './data/names.js'
+class NameGenerator {
+  constructor () {
+
+  }
+  geberateFirstName () {
+    // To do: implement first name generation logic.
+  }
+  generateLastName () {
+    // To do: implement last name generation logic.
+  }
+  generateFullName () {
+    // To do: implement full name generation logic.
+  }
+  generateUsername () {
+    // To do: implement username generation logic.
+  }
+  generateBusinessName () {
+    // To do: implement business name generation logic.
+    
+  }
+  generateEmail () {
+    // To do: implement email generation logic.
+  }
+  
+  }
+  
+  export default NameGenerator 
